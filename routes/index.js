6f5e659267750e98005f08bddc111602914b5d98
Fanta335 <fantayusuke5210@gmail.com>
2021-12-10 18:35:17 +0900

    create connection to database and get meal data

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,36 @@
+const { nextTick } = require("process");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const { promisify } = require("util");
+  const config = require("../config/mysql.config.js");
+  const mysql = require("mysql");
+  const con = mysql.createConnection({
+    host: config.HOST,
+    port: config.PORT,
+    user: config.USERNAME,
+    password: config.PASSWORD,
+    database: config.DATABASE,
+  });
+  const client = {
+    connect: promisify(con.connect).bind(con),
+    query: promisify(con.query).bind(con),
+    end: promisify(con.end).bind(con),
+  };
+  let data;
+
+  try {
+    await client.connect();
+    data = await client.query("SELECT * FROM t_meal");
+    console.log(data);
+  } catch (err) {
+    nextTick(err);
+  } finally {
+    await client.end();
+  }
+
+  // res.end("OK");
   res.render("./index.ejs");
 });
 
