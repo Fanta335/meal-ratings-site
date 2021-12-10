c493231f5fb51f2482e6cecd68e280cd89d4c63f
Fanta335 <fantayusuke5210@gmail.com>
2021-12-10 16:41:41 +0900

    create landing page draft

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use("/public", express.static(path.join(__dirname, "/public")));
 
-app.get("/", (req, res) => {
-  res.end("Hello world");
-});
+app.use("/", require("./routes/index"));
 
 app.listen(PORT, () => {
   console.log(`Application listening at http://localhost:${PORT}`);
