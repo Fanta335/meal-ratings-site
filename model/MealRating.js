ae2517eb37449a242b35ac03dfebdc1eb0dcf398
Fanta335 <fantayusuke5210@gmail.com>
2021-12-13 18:19:03 +0900

    #10 create MealRating model

---
+++ model/MealRating.js
@@ -0,0 +1,9 @@
+class MealRating {
+  constructor(meal, rating, dateOfRating){
+    this.meal = meal;
+    this.rating = rating;
+    this.dateOfRating = dateOfRating;
+  }
+}
+
+module.exports = { MealRating };
