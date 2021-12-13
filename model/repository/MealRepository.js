ec1c413e2a3b1b36d2a7f7e88e3d6311779cfc8c
Fanta335 <fantayusuke5210@gmail.com>
2021-12-13 21:40:08 +0900

    #12 create MealRepository class

---
+++ model/repository/MealRepository.js
@@ -0,0 +1,18 @@
+class MealRapository {
+  // get以外は後ほど編集する
+  addMeal = (meal) => {};
+  updateMeal = (meal) => {};
+  deleteMeal = (meal) => {};
+  getMeal = (meal) => {};
+
+  addMealRating = (mealRating) => {};
+  updateMealRating = (mealRating) => {};
+  deleteMealRating = (mealRating) => {};
+  getMealRating = (mealRating) => {};
+
+  sayhi = () => {
+    console.log("hi");
+  };
+}
+
+module.exports = { MealRapository };
