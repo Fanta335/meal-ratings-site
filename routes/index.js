d817ded6164ef230a78e3f8b577572112cfd1589
Fanta335 <fantayusuke5210@gmail.com>
2021-12-14 15:50:34 +0900

    create connection pool module

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,15 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res, next) => {
-  const { MySQLclient, sql } = require("../lib/database/client");
+  const { MySQLClient, sql } = require("../lib/database/client");
   let data;
 
   try {
-    await MySQLclient.connect();
-    data = await MySQLclient.query(sql("SELECT_MEAL.sql"), [1]);
+    data = await MySQLClient.executeQuery(sql("SELECT_MEAL.sql"), [1]);
     console.log(data);
     res.render("./index.ejs", { meals: data });
   } catch (err) {
     next(err);
-  } finally {
-    await MySQLclient.end();
   }
 });
 
