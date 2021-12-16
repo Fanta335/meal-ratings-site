c26c2188d56828b8cc0da995f7ff6a0ababc6ac1
Fanta335 <fantayusuke5210@gmail.com>
2021-12-16 19:35:10 +0900

    #17 change countryOfOrigin data type: int -> string

--- models/meal.js
+++ models/meal.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
       name: DataTypes.STRING,
       description: DataTypes.STRING,
       imageUrl: DataTypes.STRING,
-      countryOfOrigin: DataTypes.INTEGER,
+      countryOfOrigin: DataTypes.STRING,
       typicalMealTime: DataTypes.INTEGER,
     },
     {
