c2af252fa880bf25ab0c47966ca0eb0f2c4d572b
Fanta335 <fantayusuke5210@gmail.com>
2021-12-16 15:37:41 +0900

    #17 remove a column: updatedAt

--- migrations/20211216040333-create-meal.js
+++ migrations/20211216040333-create-meal.js
@@ -27,10 +27,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
       },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
     });
   },
   down: async (queryInterface, Sequelize) => {
